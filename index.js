ae9acd2a4c830e1aa64f644cbd27dda7d8454644
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-06-28 19:30:00 +0100

    x

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import { mergeSort, removeDuplicates } from "./sorting_module.js";
+
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+12;
 class Node {
   constructor(data) {
     this.data = data;
@@ -12,76 +16,20 @@ class Tree {
   }
 
   buildTree(arr) {
-    let array = arr;
-    //  sort + remove duplicates
-    let root;
+    const mid = Math.floor(arr.length / 2);
+
+    let root = arr.length == 0 ? null : new Node(arr[mid]);
+
+    if (root) {
+      root.left = this.buildTree(arr.slice(0, mid));
+      root.right = this.buildTree(arr.slice(mid + 1));
+    }
+
     return root;
   }
-
-  mergeSort(array) {
-    if (array.length < 1) {
-      return;
-    } else if (array.length == 1) {
-      return array[0];
-    }
-    let baseArr = array;
-    let middleIndex = Math.floor(array.length / 2);
-    let left = baseArr.splice(0, middleIndex);
-    let right = baseArr;
-
-    console.log(baseArr);
-    console.log(left);
-    console.log(right);
-  }
 }
 
-let arr = [22, 1, 4, 9, 2, 8, 1, 5, 8, 6, 7, 3, 22];
+let chajara = new Tree(removeDuplicates(mergeSort(arr)));
 
-function mergeSort(array) {
-  if (array.length < 1) {
-    return;
-  } else if (array.length === 1) {
-    return [array[0]];
-  }
-  const middle = Math.floor(array.length / 2);
-  const leftArr = array.slice(0, middle);
-  const rightArr = array.slice(middle);
-
-  let left = [...mergeSort(leftArr)];
-  let right = [...mergeSort(rightArr)];
-  let sortedArray = [];
-
-  let i = 0;
-  let j = 0;
-  let k = 0;
-
-  while (k < left.length + right.length) {
-    if (typeof right[j] == "undefined" || left[i] <= right[j]) {
-      sortedArray[k] = left[i];
-      k++;
-      i++;
-    } else if (typeof left[i] == "undefined" || right[j] <= left[i]) {
-      sortedArray[k] = right[j];
-      k++;
-      j++;
-    }
-  }
-
-  return sortedArray;
-}
-
-function removeDuplicates(array) {
-  let pointer = array[0];
-  let cleanArray = [array[0]];
-
-  for (let i = 1; i < array.length; i++) {
-    if (pointer !== array[i]) {
-      cleanArray.push(array[i]);
-      pointer = array[i];
-    }
-  }
-
-  return cleanArray;
-}
-console.log(mergeSort(arr));
 console.log(removeDuplicates(mergeSort(arr)));
+console.log(chajara.root);
---
+++ sorting_module.js
@@ -0,0 +1,48 @@
+export { mergeSort, removeDuplicates };
+
+function mergeSort(array) {
+  if (array.length < 1) {
+    return;
+  } else if (array.length === 1) {
+    return [array[0]];
+  }
+  const middle = Math.floor(array.length / 2);
+  const leftArr = array.slice(0, middle);
+  const rightArr = array.slice(middle);
+
+  let left = [...mergeSort(leftArr)];
+  let right = [...mergeSort(rightArr)];
+  let sortedArray = [];
+
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (k < left.length + right.length) {
+    if (typeof right[j] == "undefined" || left[i] <= right[j]) {
+      sortedArray[k] = left[i];
+      k++;
+      i++;
+    } else if (typeof left[i] == "undefined" || right[j] <= left[i]) {
+      sortedArray[k] = right[j];
+      k++;
+      j++;
+    }
+  }
+
+  return sortedArray;
+}
+
+function removeDuplicates(array) {
+  let pointer = array[0];
+  let cleanArray = [array[0]];
+
+  for (let i = 1; i < array.length; i++) {
+    if (pointer !== array[i]) {
+      cleanArray.push(array[i]);
+      pointer = array[i];
+    }
+  }
+
+  return cleanArray;
+}
